b16cb2e7b033a575163c4be5e2f0457077b17a2b
mancamdetroit-coder <mancamdetroit@gmail.com>
2026-04-16 14:00:21 -0400

    Update routes/proxy.js to export Express router with http-proxy-middleware for Google and YouTube redirects

--- routes/proxy.js
+++ routes/proxy.js
@@ -1,27 +1,18 @@
+const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-    app.use('/api/google', createProxyMiddleware({
-        target: 'https://www.googleapis.com',
-        changeOrigin: true,
-        pathRewrite: {
-            '^/api/google': '',
-        },
-        onProxyRes: function(proxyRes, req, res) {
-            delete proxyRes.headers['content-security-policy'];
-        },
-        followRedirects: true
-    }));
+const router = express.Router();
 
-    app.use('/api/youtube', createProxyMiddleware({
-        target: 'https://www.youtube.com',
-        changeOrigin: true,
-        pathRewrite: {
-            '^/api/youtube': '',
-        },
-        onProxyRes: function(proxyRes, req, res) {
-            delete proxyRes.headers['content-security-policy'];
-        },
-        followRedirects: true
-    }));
-};
+// Proxy for Google
+router.use('/google', createProxyMiddleware({
+    target: 'https://www.google.com',
+    changeOrigin: true,
+}));
+
+// Proxy for YouTube
+router.use('/youtube', createProxyMiddleware({
+    target: 'https://www.youtube.com',
+    changeOrigin: true,
+}));
+
+module.exports = router;
